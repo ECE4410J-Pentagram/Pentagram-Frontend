4dbb7d5977906eca27b8c30cb04f47f41ef18ee5
Patrick Li <baichuanli@yahoo.com>
2023-07-12 22:28:56 +0800

    feat: add key dialog ready

--- app/src/main/java/cn/edu/sjtu/patrickli/cryptex/KeyViews/KeyView.kt
+++ app/src/main/java/cn/edu/sjtu/patrickli/cryptex/KeyViews/KeyView.kt
@@ -4,28 +4,45 @@ import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.twotone.Delete
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
 import cn.edu.sjtu.patrickli.cryptex.NavBackBar
+import cn.edu.sjtu.patrickli.cryptex.R
 import me.saket.swipe.SwipeAction
 import me.saket.swipe.SwipeableActionsBox
 import java.util.function.Predicate
+
 
 @Composable
 fun KeyView(key: Key, index: Int, onRemove: (name: String) -> Unit) {
@@ -42,7 +59,9 @@ fun KeyView(key: Key, index: Int, onRemove: (name: String) -> Unit) {
             )
     {
         Column(
-            modifier = Modifier.fillMaxWidth().padding(4.dp, 8.dp, 4.dp, 4.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp, 8.dp, 4.dp, 4.dp),
         ) {
             Text(
                 text = key.name,
@@ -77,6 +96,9 @@ fun KeyViewWithDiv(key: Key, index: Int, onRemove: (name: String) -> Unit) {
 @Composable
 fun KeyListView(context: Context, navController: NavHostController) {
     var keyList = testKeys
+    var addDialogOpen by remember {
+        mutableStateOf<Boolean>(false)
+    }
     fun onRemove(name: String) {
         Log.d("KeyView","Remove Called: " + keyList.size)
         keyList.removeIf(Predicate { key -> key.name == name })
@@ -97,6 +119,27 @@ fun KeyListView(context: Context, navController: NavHostController) {
                     KeyViewWithDiv(keyList[index], index, ::onRemove)
                 }
             }
-        }
+            if (addDialogOpen) {
+                Dialog(onDismissRequest = { addDialogOpen = false }) {
+                    Card (
+                        Modifier
+                            .fillMaxWidth(1F)
+                            .defaultMinSize(minHeight = 300.dp)) {
+                        Text(text = "Under Construction")
+                    }
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 8.dp),
+                onClick = {
+                    // navigate to PostView
+                    addDialogOpen = true
+                }
+            ) {
+                Icon(Icons.Default.Add, stringResource(R.string.addKey))
+            }
+        },
     )
 }
