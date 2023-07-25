3a2f09d8cc56520761cec3f47f9ba9c01ce14114
remisiki <yangyiwen.sigo@hotmail.com>
2023-07-25 14:29:59 +0900

    fix: #10 use custom hurlstack

--- app/src/main/java/cn/edu/sjtu/patrickli/cryptex/viewmodel/RequestViewModel.kt
+++ app/src/main/java/cn/edu/sjtu/patrickli/cryptex/viewmodel/RequestViewModel.kt
@@ -2,6 +2,7 @@ package cn.edu.sjtu.patrickli.cryptex.viewmodel
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import cn.edu.sjtu.patrickli.cryptex.model.CustomHurlStack
 import cn.edu.sjtu.patrickli.cryptex.model.RequestStore
 import com.android.volley.toolbox.Volley
 
@@ -9,5 +10,5 @@ class RequestViewModel(
     val context: Context
 ): ViewModel() {
     val requestStore = RequestStore()
-    val requestQueue = Volley.newRequestQueue(context)
+    val requestQueue = Volley.newRequestQueue(context, CustomHurlStack())
 }
