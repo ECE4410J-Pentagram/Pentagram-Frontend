a3010986678567090230c4b74216d5029a01de80
remisiki <yangyiwen.sigo@hotmail.com>
2023-07-28 00:23:53 +0900

    fix: #12

--- app/src/main/java/cn/edu/sjtu/patrickli/cryptex/view/contact/SendInvitationView.kt
+++ app/src/main/java/cn/edu/sjtu/patrickli/cryptex/view/contact/SendInvitationView.kt
@@ -30,34 +30,41 @@ fun SendInvitationView(
     navController: NavHostController
 ) {
     val requestViewModel = viewModelProvider[RequestViewModel::class.java]
+    val keyViewModel = viewModelProvider[KeyViewModel::class.java]
     var showLoadingDialog by remember { mutableStateOf(false) }
+    val keyIsNotNull by remember { mutableStateOf(
+        (keyViewModel.keyToShare != null) || (keyViewModel.defaultKeyAlias != null)
+    ) }
     BaseInvitationView(
         context = context,
         viewModelProvider = viewModelProvider,
         navController = navController
     ) {
         Spacer(modifier = Modifier.height(70.dp))
-        BaseWideButton(onClick = {
-            showLoadingDialog = true
-            requestViewModel.requestQueue.add(requestViewModel.requestStore.getSendInvitationRequest(
-                viewModelProvider,
-                onResponse = {
-                    viewModelProvider[InvitationViewModel::class.java].updateInvitationList(viewModelProvider)
-                    viewModelProvider[KeyViewModel::class.java].keyToShare = null
-                    Log.d("FriendRequest", "Success")
-                    Toast.makeText(context, context.getString(R.string.addFriendSuccess), Toast.LENGTH_SHORT).show()
-                    showLoadingDialog = false
-                    navController.navigate("HomeView") { popUpTo(0) }
-                },
-                onError = {
-                    Log.d("FriendRequest", "Fail")
-                    it.printStackTrace()
-                    Toast.makeText(context, context.getString(R.string.unknownError), Toast.LENGTH_SHORT).show()
-                    showLoadingDialog = false
-                    navController.navigate("HomeView") { popUpTo(0) }
-                }
-            ))
-        }) {
+        BaseWideButton(
+            enabled = keyIsNotNull,
+            onClick = {
+                showLoadingDialog = true
+                requestViewModel.requestQueue.add(requestViewModel.requestStore.getSendInvitationRequest(
+                    viewModelProvider,
+                    onResponse = {
+                        viewModelProvider[InvitationViewModel::class.java].updateInvitationList(viewModelProvider)
+                        viewModelProvider[KeyViewModel::class.java].keyToShare = null
+                        Log.d("FriendRequest", "Success")
+                        Toast.makeText(context, context.getString(R.string.addFriendSuccess), Toast.LENGTH_SHORT).show()
+                        showLoadingDialog = false
+                        navController.navigate("HomeView") { popUpTo(0) }
+                    },
+                    onError = {
+                        Log.d("FriendRequest", "Fail")
+                        it.printStackTrace()
+                        Toast.makeText(context, context.getString(R.string.unknownError), Toast.LENGTH_SHORT).show()
+                        showLoadingDialog = false
+                        navController.navigate("HomeView") { popUpTo(0) }
+                    }
+                ))
+            }
+        ) {
             Text(text = stringResource(R.string.addFriend))
         }
         if (showLoadingDialog) {
